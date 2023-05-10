feefa34502d841cf8dd1f60e8fd825c86059acdd
albaehaki <zackyalbaehaki@gmail.com>
2023-05-10 07:20:51 +0700

    test coding

--- react-ts/src/style/footerStyle.ts
+++ react-ts/src/style/footerStyle.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const FooterStyled = styled.header`
   background-color: lightblue;
-  height: 70px;
   display: flex;
   justify-content: center;
+  padding: 25px 10px;
 `;
 
 export const TextStyled = styled.p`
-display: block;
-margin-bottom: auto;
-margin-top: auto;
-font-size: 12px;
-`
+  display: block;
+  margin-bottom: auto;
+  margin-top: auto;
+  font-size: 12px;
+`;
